55d8e9ff04f4bfc64e5731c2f901a9d218446433
Alexander <rozenrol@mail.ru>
2022-06-19 11:43:26 +0800

    Добавлены окна сообжений исправлены мелкие недачеты. Это сохранение перед глобальными изменениямию

--- definity.py
+++ definity.py
@@ -2,6 +2,10 @@ import json
 from pathlib import Path
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox as massege
+import threading
+import sys
+sys.stdout.encoding  # 'UTF-8'
 
 
 import sqlite3
@@ -14,12 +18,14 @@ class info_to_enter():
         self.list_wiget=[]
         self.name=""
         self.place=wig
+        self.path="base_data/sik.db"
+
     def get_name(self):
         return  self.name
 
     def no_name(self,):
         """
-        если имени нет то создает виджеты для получения имени
+        если имени нет, то создает виджеты для получения имени
         :return:
         """
         self.list_wiget=[]
@@ -51,10 +57,10 @@ class info_to_enter():
         записывает полученое имя в json файл
         :return:
         """
-        info = {"name_competer": self.name}
+        info = {"name_competer": self.name,"path":self.path}
         try:
-            with open("info.json", "w") as write_file:
-                json.dump(info, write_file)
+            with open("base_data/info.json", "w") as write_file:
+                json.dump(info, write_file,ensure_ascii=False)
         except:
             pass
 
@@ -63,23 +69,27 @@ class info_to_enter():
         опредиляет сохранено имя в файле, если да то дает лабел с именем на табло, иначе запускает скрспт получения имени
         """
         try:
-            with open("info.json", "r") as read_file:
+            with open("base_data/info.json", "r") as read_file:
                 info1 = json.load(read_file)
-                name_competer = info1["name_competer"]
-                self.name = name_competer
-                text = Label(text=name_competer)
+                self.name = info1["name_competer"]
+                self.path = info1["path"]
+                text = Label(text=self.name)
                 text.pack(in_=self.place, side=LEFT, ipadx=10)
 
         except:
             self.no_name()
+
 class work_sql():
-    def __init__(self):
-        self.path="base_data/sik.db"
+    def __init__(self,name,name_tabl):
+        self.path=name_tabl
+        self.name=name
+        self.admin="user"
+        self.stop=False
 
     def create_bd(self):
         with sqlite3.connect(self.path) as content:
             cursor = content.cursor()
-            cursor.execute("""CREATE TABLE IF NOT EXISTS articles(
+            cursor.execute("""CREATE TABLE IF NOT EXISTS "self.name"(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             client VARCHAR,
             topic VARCHAR,
@@ -90,20 +100,64 @@ class work_sql():
     def print_table(self):
         with sqlite3.connect(self.path) as content:
             cursor = content.cursor()
-            cursor.execute("SELECT *FROM ARTICLES")
+            cursor.execute("SELECT *FROM 'self.name'")
             return cursor.fetchall()
 
     def write_table(self,data):
+        data=self.clear_inter(data)
         with sqlite3.connect(self.path) as content:
             cursor = content.cursor()
-            cursor.execute("SELECT *FROM ARTICLES")
-            cursor.executemany("INSERT INTO articles(client, topic, text, status) VALUES(?,?,?,?)", data)
+            cursor.execute("SELECT *FROM 'self.name'")
+            if data[0][1]!="трипер":
+                self.admin = "user"
+                self.call_of_admin()
+                if data[0][0]!="":
+                    cursor.executemany("INSERT INTO 'self.name'(client, topic, text, status) VALUES(?,?,?,?)", data)
+                    massege.showerror("Отправка", "Заявка отправлена")
+
+                else:
+                    massege.showerror("Oшибка","Авторизируйтесь чтобы отправить заявку")
+            else:
+                massege.showerror("Внимание", "Вы получили права админа")
+                self.admin="admin"
+                self.call_of_admin()
+
 
     def update_info(self,data):
         with sqlite3.connect(self.path) as content:
             cursos=content.cursor()
-            cursos.execute("SELECT id  FROM articles")
-            cursos.execute("UPDATE  articles  SET status = 'Исполнено' WHERE id = (?)",[data[0]])
+            cursos.execute("SELECT id  FROM 'self.name'")
+            cursos.execute("UPDATE  'self.name'  SET status = 'Исполнено' WHERE id = (?)",[data[0]])
+
+    def clear_inter(self,list):
+        index=0
+        bow_new=[]
+        for box in list[0]:
+            word = ''
+            if index !=2:
+                for lit in box:
+                    if lit!="\n":
+                        word=word+lit
+            else:
+                word=box
+            bow_new.append(word)
+            index+=1
+        list_new=[tuple(bow_new)]
+        return list_new
+
+    def cathe_sql(self):
+        while self.stop==True:
+            print(9)
+
+    def call_of_admin(self,):
+        if self.stop==False:
+            self.stop = True
+            calling=threading.Thread(target=self.cathe_sql())
+            calling.start()
+        else:
+            self.stot=False
+
+
 
 
 
@@ -112,28 +166,39 @@ class Textery():
     Класс отвечает за вкладку 'посмотреть заявки' и создает конвас со списоком кнопок заявок ,а также текстовое поле для
     каждой заявки.Размещает эти полотна на экране меняя их между собой.
     """
-    def __init__(self, wig,sql):
+    def __init__(self, wig,sql,name):
+        self.name_computer=name
         self.place=wig
         self.sql=sql
         self.list_button_info=[]
         self.list_button=[]
         self.wiwets=[]
 
-    def count_button(self,data:list):
+    def count_button(self,data:list,):
         """
         Из полученных данных формирует информацию для будующих кнопок.
         :param data: данные из sql таблици
         :return:
         """
         self.list_button_info=[]
+        name_computer = ""
+        if self.sql.admin == "user":
+            name_computer = "user"
+        else:
+            name_computer = "admin"
+
         for i in data:
-            zip=[]
-            for ty in i :
-                ty=str(ty)
-                if '\n' in ty:
-                    ty=ty[0:-1]
-                zip.append(ty)
-            self.list_button_info.append(zip)
+                zip=[]
+                for biter in i :
+                    word=""
+                    for liter in str(biter):
+                        if liter!="\n":
+                            word=word+liter
+                    if i[1] == self.name_computer or name_computer == "admin":
+                        zip.append(word)
+                if zip:
+                    self.list_button_info.append(zip)
+
 
 
     def create_button(self,):
@@ -208,6 +273,7 @@ class Textery():
         tex_teria.pack(side=LEFT,fill=BOTH,expand=1)
         skrol_text.pack(side=RIGHT,fill=Y)
 
+
     def update_status(self,data) :
         """
         обновляет статус заказа в таблице и для кнопок
@@ -218,6 +284,12 @@ class Textery():
         self.create_button()
         data[4]="Исполнено"
         self.put_text(data)
+        massege.showerror("Изменение", "Заявка отмечена как 'выполенно'")
+
+    def click_to_notebook(self):
+        self.count_button(self.sql.print_table())
+        self.create_button()
+        self.put_button()
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import threading
 import datetime
 import json
 import definity
@@ -8,36 +9,17 @@ window.title("Программа оформления заявок")
 width = 660
 height = 500
 
-sql_table=definity.work_sql()
-sql_table.create_bd()
+
 window.geometry(f"{width}x{height}")
 date=datetime.datetime.today()
 date=definity.kalendwr([date.day, date.month,date.year])
 
-
-
-
 #поля
-#definity.chenge_wiget(window,"no")
-#создать заявку
-#make_oder=definity.WidgetShow()
 
 
 #посмотреть заявку
 
-
-
-
-
-
-
-
 #надписи
-
-
-
-
-
 
 #кнопки
 
@@ -80,37 +62,23 @@ klient.pack(side=LEFT ,ipadx=10)
 meet=definity.info_to_enter(frame_info)
 meet.chenge_wiget()
 tab1.place()
-
+sql_table=definity.work_sql(meet.name,meet.path)
+sql_table.create_bd()
 tabControl.add(tab2, text='Посмотреть заявки')
 
 #вкладка2
 frame_tab2=Frame(tab2)
-#frame_select=Frame(frame_tab2)
-#skroll_button=ttk.Combobox(values=definity.return_topic(sql_table.print_table()))
-#skroll_button.bind("<Enter>",  lambda e:definity.chenge_wiget_info(skroll_button,definity.return_topic(sql_table.print_table())))
-
-
-#skroll_button.pack(in_=frame_select,anchor=NW,expand=1,fill=BOTH,side=LEFT)
-#frame_select.pack(fill=X,)
-
 frame_text_oders=Frame(frame_tab2,)
-#text_oders=Text(frame_text_oders,)
-buttib=definity.Textery(frame_text_oders,sql_table)
-buttib.count_button(sql_table.print_table())
-buttib.create_button()
-buttib.put_button()
+buttib=definity.Textery(frame_text_oders,sql_table,meet.name)
 
-#scrol_text_oders=Scrollbar(frame_text_oders)
-#text_oders.pack(in_=frame_text_oders,anchor=NE,expand=1,fill=X,side=LEFT)
-#scrol_text_oders.pack(in_=frame_text_oders,fill=Y,side=RIGHT)
-#text_oders.config(yscrollcommand=scrol_text_oders.set)
 frame_text_oders.pack(anchor=NW,expand=1,fill=BOTH,)
 frame_tab2.place(relx=0, rely=0,relwidth=1, relheight=1 )
-#frame_tab2.bind("<Enter>", lambda e: definity.return_info_in_sql(skroll_button.get(),sql_table.print_table(),text_oders))
+
 
 
 
 tab2.place()
 tabControl.place(relx=0, rely=0,relwidth=1, relheight=1 )
+tabControl.bind('<Button-1>',lambda e: buttib.click_to_notebook())
 
 window.mainloop()
